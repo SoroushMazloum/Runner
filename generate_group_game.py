2204ae0d063eeb494ae4fc5498c10567444949eb
Soroush Mazloum <mazloomsoroush@gmail.com>
2025-06-17 20:11:09 +0330

    group generator error

--- generate_group_game.py
+++ generate_group_game.py
@@ -33,4 +33,6 @@ if __name__ == "__main__":
                 os.system(f"echo '{teams[j + i]}' >> Games.txt")
                 os.system(f"echo '---' >> Games.txt")
         
-        print("Games generated [\033[32mSuccess\033[0m]")
+    
+    else:
+        print("\033[31mteams.txt is empty \033[0m[\033[31mERROR\033[0m]")
