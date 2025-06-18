578485b05fa1ad9b85142d07ab819faaa54dd31a
Soroush Mazloum <mazloomsoroush@gmail.com>
2025-06-18 13:12:09 +0330

    removed analyzer, runner will merge to master

--- Analyzer/ANALYZE.sh
+++
@@ -1,6 +0,0 @@
-source .venv/bin/activate
-python3 Analyzer/graph_bar_points.py
-python3 Analyzer/match_graph_with_winners.py
-./convert_logs.sh
-./generate_reports.sh
-python3 tactical_analysis.py
--- Analyzer/LogsJSON/README.md
+++
@@ -1,1 +0,0 @@
-# Raw game analysis
