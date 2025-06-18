822419695e4631b1333b42f1c5c07ce802c4cccf
Soroush Mazloum <mazloomsoroush@gmail.com>
2025-06-18 13:09:42 +0330

    edit: group_generator.py

--- group_generator.py
+++ group_generator.py
@@ -32,6 +32,7 @@ if __name__ == "__main__":
                 os.system(f"echo '{teams[j]}' >> Games.txt")
                 os.system(f"echo '{teams[j + i]}' >> Games.txt")
                 os.system(f"echo '---' >> Games.txt")
+        print(f"Games generated and added to Games.txt [\033[32mSuccess\033[0m]")
         
     
     else:
